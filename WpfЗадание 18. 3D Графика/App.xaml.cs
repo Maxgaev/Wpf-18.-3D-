d8417e8c8fca31b8b5bc9a23ec3352ef9f5485e6
Максим Алехин <Max-crf@mail.ru>
2022-02-26 12:23:39 +0700

    Добавьте файлы проекта.

---
+++ WpfЗадание 18. 3D Графика/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfЗадание_18._3D_Графика
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
